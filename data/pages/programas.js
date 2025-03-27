70a85c69da2299b10879c5af1b5304e988eb7dae
KenQui3312 <kenetadiel@gmail.com>
2025-03-27 13:40:02 -0600

    agg info proyectos

--- data/pages/programas.js
+++ data/pages/programas.js
@@ -4,19 +4,19 @@ export const programas={
             imgur:'',
             imgdsc:'foto1',
             prname:'Programa de apoyo para investigadores hondureños',
-            prdsc:'Aves Honduras (ASHO) lanzó este proyecto bandera en julio 2020, para promover una de sus cuatro áreas principales – la investigación científica. La ornitología es'
+            prdsc:'Aves Honduras (ASHO) lanzó este proyecto bandera en julio 2020, para promover una de sus cuatro áreas principales – la investigación científica. La ornitología es el estudio de las aves y las investigaciones que puedan contribuir al conocimiento de las aves son importantes ya que las aves tienen impactos sobre las actividades humanas y sobre los ecosistemas. Por su facilidad de observación (comparada con otros grupos taxonómicos) y su presencia global, las aves son frecuentemente utilizadas como indicadores de la salud de un ecosistema. Además, las aves son un excelente modelo para comprender preguntas científicas en otras disciplinas como la ecología y biología evolutiva: ¿Porqué existen partes del mundo con más diversidad biológica que otras? ¿Cómo pueden co-existir distintas especies en un mismo espacio?'
         },
         {
             imgur:'',
             imgdsc:'foto2',
-            prname:'Programa de apoyo para investigadores hondureños',
-            prdsc:'Aves Honduras (ASHO) lanzó este proyecto bandera en julio 2020, para promover una de sus cuatro áreas principales – la investigación científica. La ornitología es'
+            prname:'Talleres educativos sobre aves',
+            prdsc:'Aves Honduras (ASHO) lanza este proyecto bandera en conmemoración de sus 10 años de creación (17 de julio 2020), para promover una de sus cuatro áreas principales – la educación ambiental. Se conoce que las aves son idóneas para realizar proyectos de educación ambiental, ya que se encuentran en cualquier parte, son llamativas y son buenos indicadores para medir la salud de los ecosistemas. Este proyecto incluye talleres de capacitación para docentes y maestros de escuelas y actividades educativas para jóvenes, como campamentos dónde aprenden a identificar aves, y usar plataformas de ciencia participativa.'
         },
         {
             imgur:'',
             imgdsc:'foto3',
-            prname:'Programa de apoyo para investigadores hondureños',
-            prdsc:'Aves Honduras (ASHO) lanzó este proyecto bandera en julio 2020, para promover una de sus cuatro áreas principales – la investigación científica. La ornitología es'
+            prname:'Búsqueda de aves en sitios poco explorados de Honduras',
+            prdsc:'En el marco del 10mo aniversario Aves Honduras (ASHO), este día lanzamos el proyecto bandera “Búsqueda de aves en sitios poco explorados de Honduras».  El proyecto ayudará a fortalecer tanto el conocimiento ornitológico del país, como la capacidad instalada para el fomento de aviturismo nacional e internacional. Generará oportunidades para la responsabilidad social y ambiental de las empresas, que esperamos hagan contribuciones periódicas al proyecto o programa. Igualmente generará nuevos beneficios para los miembros contribuyentes de la Asociación, que podrían ser beneficiados con participaciones en giras exploratorias a los rincones silvestres y poco conocidos de Honduras.'
         },
 
     ]
