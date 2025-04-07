6ad27cdbb9e1a55b095e47710dc46cf5a72adff0
KenQui3312 <kenetadiel@gmail.com>
2025-04-07 16:53:50 -0600

    commit 6 7-4-25

--- data/pages/blog.js
+++ data/pages/blog.js
@@ -1,21 +1,21 @@
 export const blog = {
   cardsBlog: [
     {
-      src: "../../media/imagen1.png",
+      src: "media/imagen1.png",
       alt: "Asociación Hondureña de Ornitología",
       titulo: "¿Quiénes somos?",
       precio: "",
       descripcion: "La Asociación Hondureña de Ornitología (AHO) es una organización dedicada al estudio, conservación y divulgación de las aves en Honduras. Fundada por un grupo de apasionados observadores de aves, promovemos la educación ambiental y la ciencia ciudadana.",
     },
     {
-      src: "../../media/imagen2.png",
+      src: "media/imagen2.png",
       alt: "Observación de aves",
       titulo: "Actividades principales",
       precio: "",
       descripcion: "Organizamos salidas de observación de aves, talleres educativos, monitoreo de especies y colaboraciones científicas. Nuestro objetivo es fomentar el amor por la naturaleza a través del conocimiento de las aves.",
     },
     {
-      src: "../../media/imagen3.png",
+      src: "media/imagen3.png",
       alt: "Ave emblemática de Honduras",
       titulo: "El ave nacional: Guacamaya Roja",
       precio: "",
